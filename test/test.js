1930874faa950ddb9c2601ac6a981e3f8ab1a25e
Greg V <greg@unrelenting.technology>
2015-08-02 22:33:43 +0300

    use tape

--- test/test.js
+++ test/test.js
@@ -1,17 +1,15 @@
 'use strict'
 
-require('should')
+var test = require('tape')
 var fs = require('fs')
 var path = require('path')
 var broccoli = require('broccoli')
 var rsvg = require('../')
 
-describe('broccoli-rsvg', function() {
-	it('should create a png file throw errors', function(done) {
-		var builder = new broccoli.Builder(rsvg('test/svg'))
-		builder.build().then(function(results) {
-			fs.existsSync(path.join(results.directory, 'test.png')).should.be.true()
-			builder.cleanup().then(function() { done() })
-		}).catch(done)
-	})
+test('rsvg', function(t) {
+	t.plan(1)
+	var builder = new broccoli.Builder(rsvg('test/svg'))
+	builder.build().then(function(results) {
+		t.ok(fs.existsSync(path.join(results.directory, 'test.png')), 'creates a png file')
+	}).catch(t.end)
 })
